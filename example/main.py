54dc4f8b55f8cb00cdce5f2c7eb6b80a5a20a994
S_Remi <S_Remi@outlook.com>
2023-07-30 08:32:33 +0900

    #9 Fix the example file to be more detailed

--- example/main.py
+++ example/main.py
@@ -1,3 +1,30 @@
+import tempfile
+import urllib.parse
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
 import easyscraping
 
-driver = easyscraping.SeleniumBrowserDriver()
+
+def google_search(search_text):
+    with tempfile.TemporaryDirectory() as directory_name:
+        with easyscraping.SeleniumBrowserDriver(work_dir_path=directory_name, is_headless=True) as driver:
+            url = 'https://www.google.com/search?q='
+            url += urllib.parse.quote_plus(search_text)
+
+            driver.sleep_requests(url)
+            driver.window_resize()
+            try:
+                search_results = driver.find_elements(By.CSS_SELECTOR, '#rso > div')
+                url_list = []
+                for search_result in search_results:
+                    link = search_result.find_element(By.CSS_SELECTOR, 'div > div > div:nth-child(1) > div > a')
+                    url_list.append(link.get_attribute('href'))
+                return url_list
+            except NoSuchElementException:
+                return []
+
+
+if __name__ == '__main__':
+    print(google_search('python'))
